f9bbf52ec8a872c7daf9a939b85fec58bf0082b7
sevozkann <54276317+sevozkann@users.noreply.github.com>
2023-01-15 23:02:52 +0300

    food-2:added next image

--- components/products/MenuWrapper.jsx
+++ components/products/MenuWrapper.jsx
@@ -7,10 +7,10 @@ const MenuWrapper = () => {
     <div className='px-5 mx-auto pb-5 xl:container'>
         <Title addClass={`text-center text-4xl`}>Our Menu</Title>
         <div className='flex justify-center items-center'>
-            <button className='px-6 py-2 rounded-3xl bg-secondary text-white'>All</button>
-            <button className='px-6 py-2 rounded-3xl'>Burger</button>
-            <button className='px-6 py-2 rounded-3xl'>Pizza</button>
-            <button className='px-6 py-2 rounded-3xl'>Drink</button>
+            <button className='px-4 py-1 rounded-3xl bg-secondary text-white'>All</button>
+            <button className='px-4 py-1 rounded-3xl'>Burger</button>
+            <button className='px-4 py-1 rounded-3xl'>Pizza</button>
+            <button className='px-4 py-1 rounded-3xl'>Drink</button>
         </div>
         <div className='mt-8 grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4'>
             <MenuItem />
